fba4bd1104d70a402dfda39dd0fa838a31eb9186
seubpong.mon <pjame.fb@gmail.com>
2022-04-14 11:38:16 +0700

    Use for_each

--- 01-1-gce-nodes/gce-nodes.tf
+++ 01-1-gce-nodes/gce-nodes.tf
@@ -1,19 +1,19 @@
 module "rke-masters" {
-  for_each = toset(var.master_nodes)
+  for_each = {for vm in var.master_nodes: vm.sequence => vm}
 
   source          = "../modules/gce"
 
-  vm_name         = "${var.vm_master_name_prefix}-${each.key}"
+  vm_name         = "${var.vm_master_name_prefix}"
   vm_sequence     = "${each.value.sequence}"
   vm_tags         = ["dummy"]
   vm_service_account = var.gce_rke_service_account
   boot_disk_image  = "will-change"
   vm_machine_type  = "e2-small"
-  vm_zone          = "${var.region}-a"
+  vm_zone          = "${var.region}-${each.value.zone}"
   vm_user          = "devops"
-  vm_subnet        = "rke-demo-subnet-001"
+  vm_subnet        = var.vpc_subnet
   vm_ext_disk_size = 100 # 100GB
   project = var.project
   startup_script_path = "scripts/startup.bash"
-  private_ip       = "192.168.1.1"
+  private_ip       = "${each.value.ip}"
 }
