bfce768de0d3bd6553db0c04c7068b87999cae14
seubpong.mon <pjame.fb@gmail.com>
2022-04-14 15:53:31 +0700

    Set allow firewall port

--- 00-2-firewall/firewall.tf
+++ 00-2-firewall/firewall.tf
@@ -6,9 +6,23 @@ resource "google_compute_firewall" "rke-inter-connect" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443", "6443"] #More to add
+    ports    = ["443", "6443"] #More to add
   }
 
   source_tags = ["rke-worker", "rke-master"]
+  target_tags = ["rke-worker", "rke-master"]
+}
+
+resource "google_compute_firewall" "rke-manager-connect" {
+  name    = "rke-manager-connect-allow"
+  network = var.vpc_name
+  priority = 1000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443", "6443", "30000-40000"]
+  }
+
+  source_tags = ["rke-manager"]
   target_tags = ["rke-worker", "rke-master"]
 }
