5cf778769a09314878a803c21060f5a8d3e3dd1a
seubpong.mon <pjame.fb@gmail.com>
2022-04-14 16:59:04 +0700

    Set sensitive = true

--- 02-1-rke-cluster/rke-output.tf
+++ 02-1-rke-cluster/rke-output.tf
@@ -1,7 +1,9 @@
 output "kube_config_yaml" {
   value = rke_cluster.rke-cluster.kube_config_yaml
+  sensitive = true
 }
 
 output "rke_state" {
   value = rke_cluster.rke-cluster.rke_state
+  sensitive = true
 }
